9dccbacace6e495d63de9be523b17165314f0b5c
Viktharien <viktharien@zoho.com>
2018-05-15 16:30:42 +0000

    test declarations on db

--- app/controllers/declarations_controller.rb
+++ app/controllers/declarations_controller.rb
@@ -1,7 +1,7 @@
 class DeclarationsController < ApplicationController
   
   def create
-    @declaration = Declaration.new(declarations_params)
+    @declaration = Declaration.new(declarations_params.to_hash)
     if @declaration.save
       output = {
         message: "Success"

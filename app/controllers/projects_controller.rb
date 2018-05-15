78ea4b3f5cfab333d8c229457267274564eb1467
Viktharien <viktharien@zoho.com>
2018-05-15 16:39:03 +0000

    test declarations on db

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -13,7 +13,7 @@ class ProjectsController < ApplicationController
   end
   
   def create
-    @project = Project.new(project_params)
+    @project = Project.new(projects_params)
     if @project.save
       redirect_to @project
     else

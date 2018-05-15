746c4d369a2d5211c92ff93cf97ba4efbfafec8e
Viktharien <viktharien@zoho.com>
2018-05-15 15:40:41 +0000

    gen projects model

---
+++ db/migrate/20180515154039_create_projects.rb
@@ -0,0 +1,12 @@
+class CreateProjects < ActiveRecord::Migration[5.1]
+  def change
+    create_table :projects do |t|
+      t.string :title
+      t.text :frameworks
+      t.text :languages
+      t.text :gems
+
+      t.timestamps
+    end
+  end
+end

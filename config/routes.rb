62dfbc7f860ddc20987c4fe486d4b6239bf4dc12
Viktharien <viktharien@zoho.com>
2018-05-15 15:34:12 +0000

    gen pages controller and set up actions

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,4 @@ Rails.application.routes.draw do
   get 'statics/contact'
 
   mount Hyperloop::Engine => '/hyperloop'
-  # root 'hyperloop#app' # route just the root to the component named App
-  get '/(*other)', to: 'hyperloop#app' # route everything to the App component
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
